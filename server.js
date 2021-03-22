7257de5d4e7c595cc8eb7c8794b71169b1c83096
OisinFoley <oisinfoley@yahoo.co.uk>
2021-03-22 12:05:16 +0100

    updates

--- server.js
+++ server.js
@@ -7,34 +7,35 @@ const fs = require('fs');
 // Array of Mime Types
 const mimeTypes = {
   // Text Types
-  "html" : "text/html",
-  "css" : "text/css",
-  "js" : "text/javascript",
+  html: 'text/html',
+  css: 'text/css',
+  js: 'text/javascript',
   // Image Types
-  "jpeg" : "image/jpeg",
-  "jpg" : "image/jpeg",
-  "png" : "image/png",
-  "gif" : "image/gif",
-  "webp" : "image/webp",
-  "svg" : "image/svg+xml",
-  "icon" : "image/x-icon",
+  jpeg: 'image/jpeg',
+  jpg: 'image/jpeg',
+  png: 'image/png',
+  gif: 'image/gif',
+  webp: 'image/webp',
+  svg: 'image/svg+xml',
+  icon: 'image/x-icon',
   // Audio and Video Types
-  "webm" : "video/webm",
-  "ogg" : "video/ogg",
-  "mp4" : "video/mp4",
-  "mp3" : "audio/mpeg",
+  webm: 'video/webm',
+  ogg: 'video/ogg',
+  mp4: 'video/mp4',
+  mp3: 'audio/mpeg',
   // Font Types
-  "ttf" : "font/ttf",
-  "otf" : "font/otf",
-  "woff" : "font/woff",
-  "woff2" : "font/woff2",
+  ttf: 'font/ttf',
+  otf: 'font/otf',
+  woff: 'font/woff',
+  woff2: 'font/woff2',
   // Application Types
-  "pdf" : "application/pdf"
+  pdf: 'application/pdf',
 };
 
 // Hostname and Port
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = 'localhost';
+// const port = 8080;
+const port = process.env.PORT || 3000;
 
 // Create Server
 const server = http.createServer((req, res) => {
@@ -45,22 +46,22 @@ const server = http.createServer((req, res) => {
 
   try {
     stats = fs.lstatSync(fileName);
-  } catch(e) {
+  } catch (e) {
     // If file not found
-    res.writeHead(404, {'Content-Type' : 'text/plain'});
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.write('404 not Found\n');
     res.end();
     return;
   }
 
   // Check if file or directory
-  if(stats.isFile()) {
+  if (stats.isFile()) {
     var mimeType = mimeTypes[path.extname(fileName).split('.').reverse()[0]];
     res.statusCode = 200;
     res.setHeader('Content-Type', mimeType);
     var fileStream = fs.createReadStream(fileName);
     fileStream.pipe(res);
-  } else if(stats.isDirectory()) {
+  } else if (stats.isDirectory()) {
     res.statusCode = 302;
     res.setHeader('Location', 'index.html');
     res.end();
@@ -69,10 +70,9 @@ const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/plain');
     res.end('500 Internal Error\n');
   }
-
 });
 
 // Run Server
-server.listen(port, hostname, () => {
+server.listen(port, () => {
   console.log('Server running at http://' + hostname + ':' + port + '\n');
 });
